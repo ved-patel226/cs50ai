55f078bfc1c9e72818349fcb564c59c1bc692898
Ved Patel <158240030+ved-patel226@users.noreply.github.com>
2024-07-25 11:54:26 -0400

    Delete python directory

--- python/random os stuff/finding path to files/main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ppath = r"Z:\\Documents\\Ved\\github scratchpad"
-path = r"Z:\Documents\Ved\github scratchpad\very very old projects\Password Generator That Stores Passwords\data.csv"
-
-relative_path = os.path.relpath(path, ppath)
-print(relative_path)
--- python/tensorflow/image classification/test.py
+++
@@ -1,5 +0,0 @@
-import time
-from alive_progress import alive_it
-
-for item in alive_it(range(100), spinner="waves"):
-    time.sleep(0.01)
